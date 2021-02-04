687597d8e73299fde4634b20a4a8f973da8d03d1
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-02-04 20:57:15 +0530

    uploading code

---
+++ src/components/Quotes/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import quotes from '../../db/quotes';
+
+export default function QuoteAndcharacter(props) {
+  const { quote, generateRandomQuote } = props;
+  return (
+    <div id='quote-box'>
+      <div id='text-div'>
+        <i className='fas fa-quote-left open quote'></i>
+        <div id='text'>
+          <p>
+            {quote.quote}
+          </p>
+        </div>
+        <p id='author'>-{quote.author}</p>
+      </div>
+      <button
+        id='new-quote'
+        onClick={() => {
+          generateRandomQuote(quotes);
+        }}
+      >
+        <i className='fas fa-forward'></i>
+      </button>
+      <a href='a' id='tweet-quote' target='blank'>
+        <button
+          id='twitter'
+          onClick={() => {
+            window.open(
+              'https://twitter.com/intent/tweet/?text=' +
+                encodeURIComponent(quote.quote + ' -- ' + quote.author)
+            );
+          }}
+        >
+          <i className='fab fa-twitter'></i>
+        </button>
+      </a>
+    </div>
+  );
+}
